91cf46f51777b3bba1553cdf06d57a9e65ad3320
CP <c.bezemer@tudelft.nl>
2014-06-24 16:52:12 +0200

    fixed dispersyreporter2html.py now parses multiple stack traces from one report

--- parsers/dispersyreporter2html.py
+++ parsers/dispersyreporter2html.py
@@ -13,7 +13,7 @@ import os
 import sys
 import getopt
 import logging
-
+from StringIO import StringIO
 
 class ExceptionLogParser(object):
 
@@ -21,27 +21,44 @@ class ExceptionLogParser(object):
         super(ExceptionLogParser, self).__init__()
 
         self._logger = logging.getLogger(self.__class__.__name__)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+        self._logger.addHandler(ch)
 
     def process_report(self, pkg_path, report_dir, to_overwrite):
         """Parses a given package and creates a report out of it.
         """
         # ignore existing report
         report_title = os.path.splitext(os.path.basename(pkg_path))[0]
-        report_filename = report_title + u".html"
+        report_filename = u"%s.html" % (report_title)
         report_filepath = os.path.join(report_dir, report_filename)
-
+        trace = 0
         if not to_overwrite and os.path.exists(report_filepath):
             return
+        
         content = self.__parse_bz2pkg(pkg_path)
         if not content:
             return
 
-        xml_data_dict = self.__create_report(content)
-        if not xml_data_dict:
-            return
-
+        in_s = StringIO(content)
+        
+        # init html report
         creator = HTMLReportCreator()
-        creator.create(report_title, xml_data_dict, report_filepath, to_overwrite)
+        creator.create(report_title, report_filepath, to_overwrite)
+                        
+        while True:
+            try:
+                xml_data_dict = self.__create_report(in_s)
+                if not xml_data_dict:
+                    return
+            except:
+                break
+            else:        
+                creator.append("Stack #%d" % trace, xml_data_dict)
+                trace = trace+1
+        
+        creator.write(report_filepath)
+            
 
     def __parse_bz2pkg(self, pkg_path):
         """Parses a bzip2 packge of an exception report and returns a data
@@ -65,14 +82,14 @@ class ExceptionLogParser(object):
 
         return content
 
-    def __create_report(self, content):
+    def __create_report(self, content_stream):
         """Creats a report out of a given content. It returns a dict for XML.
         """
-        try:
-            raw_data_dict = pickle.loads(content)
-        except:
-            self._logger.exception(u"Failed to load pickle content [%s]", content)
-            return
+        #try:
+        raw_data_dict = pickle.load(content_stream)
+        #except:
+        #    self._logger.exception(u"Failed to load pickle content [%s]", content_stream)
+        #    raise EOFError            
 
         # get fields
         xml_data_dict = {}
@@ -100,41 +117,46 @@ class HTMLReportCreator(object):
         super(HTMLReportCreator, self).__init__()
 
         self._logger = logging.getLogger(self.__class__.__name__)
+        self.title = ""
+        self.html_content = ""
 
-    def create(self, title, data_dict, filepath, to_overwrite):
+    def create(self, title, filepath, to_overwrite):
         """Creates an HTML report from a give data dict.
         """
         if os.path.exists(filepath) and not to_overwrite:
             return
+        self.title = u"Report-%s" % title
 
-        title = u"Report-%s" % title
+        self.html_content = u"<html>\n"
 
-        html_content = u"<html>\n"
+        self.html_content += u"<head>\n"
+        self.html_content += u"  <title>Report overview for %s</title>\n" % title
+        self.html_content += u"</head>\n"
+        self.html_content += u"<body>\n"
 
-        html_content += u"<head>\n"
-        html_content += u"  <title>%s</title>\n" % title
-        html_content += u"</head>\n"
-
-        html_content += u"<body>\n"
-        html_content += u"  <h1>%s</h1>\n" % title
-        html_content += u"  <table border=\"1\">\n"
+    def append(self, title, data_dict):
+        self.html_content += u"  <h1>%s</h1>\n" % title
+        self.html_content += u"  <table border=\"1\">\n"
         for kw, val in data_dict.iteritems():
             if kw in (u"sysinfo", ):
                 continue
-            html_content += u"  <tr>\n"
-            html_content += u"    <th>%s</th>\n" % kw
+            self.html_content += u"  <tr>\n"
+            self.html_content += u"    <th>%s</th>\n" % kw
             content = unicode(val).replace('\n', '<br/>')
-            html_content += u"    <td>%s</td>\n" % content
-            html_content += u"  </tr>\n"
-        html_content += u"  </table>\n"
-        html_content += u"</body>\n"
+            self.html_content += u"    <td>%s</td>\n" % content
+            self.html_content += u"  </tr>\n"
+        self.html_content += u"  </table>\n"
+        self.html_content += u"<hr>\n"
+        
 
-        html_content += u"</html>\n"
-
+    def write(self, filepath):
         outfile = None
         try:
             outfile = codecs.open(filepath, 'wb', 'utf-8')
-            outfile.write(html_content)
+            self.html_content += u"</body>\n"
+
+            self.html_content += u"</html>\n"   
+            outfile.write(self.html_content)
         except:
             self._logger.exception(u"Failed to write to file [%s]", filepath)
         finally:
