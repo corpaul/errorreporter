756f412baecb05741f6579bf9d427cb3b86368ad
CP <c.bezemer@tudelft.nl>
2014-07-11 16:33:44 +0200

    add indexes

--- djangoproject/errorreporter/models.py
+++ djangoproject/errorreporter/models.py
@@ -7,9 +7,9 @@ class CrashReport(models.Model):
     timestamp = models.CharField(max_length=200, unique=True)
     sysinfo = models.TextField()
     comments = models.CharField(max_length=300)
-    stack = models.TextField()
-    version = models.CharField(max_length=10)
-    date = models.DateField()
+    stack = models.TextField(db_index=True)
+    version = models.CharField(max_length=10, db_index=True)
+    date = models.DateField(db_index=True)
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return "%s: Version %s\n %s\n %s\n" % (self.timestamp, self.version, self.stack, self.comments)
