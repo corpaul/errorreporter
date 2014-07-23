16b90b150b6064919abd019920f45d78f2cb6a12
CP <c.bezemer@tudelft.nl>
2014-07-23 09:30:05 +0200

    merge aggr report templates

--- djangoproject/errorreporter/views.py
+++ djangoproject/errorreporter/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 from django.db.models import Count
 from errorreporter.models import CrashReport
 import time
-import operator
+
 
 # Create your views here.
 def index(request):
@@ -29,16 +27,17 @@ def crashreport_daily(request, date):
     comments = compact_comments(crashreports)
     objects = CrashReport.objects.values('stack').filter(date=date)
     crashreports_aggr = objects.annotate(cnt=Count('stack')).order_by('-cnt')
-    
+
     for c in crashreports_aggr:
         tmp = CrashReport.objects.filter(stack=c['stack']).first()
         c['id'] = tmp.id
         c['comments'] = comments[c['stack']]
-        
+
     context = {'crashreports': crashreports,
                'crashreports_aggr': crashreports_aggr,
-               'date': date}
-    return render(request, 'errorreporter/crashreport_daily.html', context)
+               'report_for': date,
+               'fg_prefix': "fg_d" + date}
+    return render(request, 'errorreporter/crashreport_aggr.html', context)
 
 
 def crashreport_version(request, version):
@@ -46,51 +45,57 @@ def crashreport_version(request, version):
     comments = compact_comments(crashreports)
     objects = CrashReport.objects.values('stack').filter(version=version)
     crashreports_aggr = objects.annotate(cnt=Count('stack')).order_by('-cnt')
-    
+
+    os_objects = CrashReport.objects.values('os').filter(version=version)
+    os_info = os_objects.annotate(cnt=Count('os')).order_by('-cnt')
+
     # add some extra info to the aggregate reports
     for c in crashreports_aggr:
         tmp = CrashReport.objects.filter(stack=c['stack']).first()
         c['id'] = tmp.id
         c['comments'] = comments[c['stack']]
-    
+
     formattedversion = version.replace(".", "_")
-    
+
     context = {'crashreports': crashreports,
                'crashreports_aggr': crashreports_aggr,
-               'version': version,
-               'formattedversion': formattedversion}
-    return render(request, 'errorreporter/crashreport_version.html', context)
- 
+               'report_for': version,
+               'fg_prefix': "fg_v" + formattedversion,
+               'os_info': os_info}
+    return render(request, 'errorreporter/crashreport_aggr.html', context)
+
+
 def graph_stack_occurrences(request, stack_id):
     objects = CrashReport.objects.filter(id=stack_id)
     stack = objects.first()
-    
+
     if stack:
         objects = CrashReport.objects.values('date').filter(stack=stack.stack)
         occurrences = objects.annotate(cnt=Count('date')).order_by('date')
     else:
         occurrences = None
-    
+
     total = 0
     for o in occurrences:
         dtt = o['date'].timetuple()
         ts = time.mktime(dtt)
-        o['ts'] = int(ts)*1000
+        o['ts'] = int(ts) * 1000
         total = total + o['cnt']
-        
+
     context = {
                'occurrences': occurrences,
-               'total': total 
+               'total': total
                }
     return render(request, 'errorreporter/graph_stack_occurrences.html', context)
-            
+
+
 def stacktrace(request, stack_id):
     objects = CrashReport.objects.filter(id=stack_id)
     stack = objects.first()
     context = {'c': stack}
     return render(request, 'errorreporter/stacktrace.html', context)
-            
-            
+
+
 def compact_comments(objects):
     """
     Collect the comments for each stack trace in a more compact fashion, i.e.:
@@ -100,8 +105,7 @@ def compact_comments(objects):
     for o in objects:
         if not o.stack in comments.keys():
             comments[o.stack] = {}
-        if not o.comments in comments[o.stack].keys():        
+        if not o.comments in comments[o.stack].keys():
             comments[o.stack][o.comments] = []
         comments[o.stack][o.comments].append(o.id)
-    return comments 
-        
+    return comments
